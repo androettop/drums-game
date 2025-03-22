7a59f455baea640c10c2d70eac816cb320bc63d5
Pablo Androetto <androettop@gmail.com>
2025-03-22 18:55:50 +0100

    Image loader for cover

--- src/components/Highway/engine.ts
+++ src/components/Highway/engine.ts
@@ -2,13 +2,14 @@ import { Color, Engine } from "excalibur";
 import { SongData } from "../../types/songs";
 import { Resources as NotesResources } from "./actors/resources";
 import { GAME_CONFIG } from "./config";
-import { MusicFile } from "./helpers/loaders";
+import { ImageFile, MusicFile } from "./helpers/loaders";
 import { createLoader } from "./resources";
 import MainScene from "./scenes/MainScene";
 
 class HighwayEngine extends Engine {
   song: SongData;
   audioTracks: MusicFile[] = [];
+  cover: ImageFile | null = null;
 
   constructor(canvas: HTMLCanvasElement, song: SongData) {
     super({
@@ -22,9 +23,11 @@ class HighwayEngine extends Engine {
   }
   initialize() {
     this.audioTracks = this.song.audioFileData.songTracks.map((trackName) => new MusicFile(this.song, trackName));
+    this.cover = new ImageFile(this.song, this.song.recordingMetadata.coverImagePath);
     this.add("main", new MainScene());
     const loader = createLoader(NotesResources);
     loader.addResources(this.audioTracks);
+    loader.addResource(this.cover);
     this.start(loader);
   }
 
